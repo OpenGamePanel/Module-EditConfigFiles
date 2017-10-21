96028098b0f9816f501e208f0ffcb5e733ec193f
own3mall <own3mall@gmail.com>
2017-10-21 16:19:53 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Arabic/modules/editconfigfiles.php
+++ lang/Arabic/modules/editconfigfiles.php
@@ -3,7 +3,7 @@
 define('configuration_files', "ملفات التكوين");
 define('editing_file', "تعديل الملف");
 define('no_server_specfied', "No Server ID specified - can't list any configuration files.");
-define('no_home', "You don't have access to the given Server ID.");
+define('no_home', "ليس لديك حق الوصول إلى معرف الخادم المعين.");
 define('no_configs_for_game', "لم يتم تحديد ملفات التكوين هذه.");
 define('name', "الاسم");
 define('no_description', "بدون وصف");
@@ -14,4 +14,4 @@ define('failed_write', "أخفق حفظ الملف.");
 define('failed_read', "أخفق قراءة الملف.");
 define('save', "حفظ");
 define('go_back', "رجوع");
-define('new_file', "New File");
+define('new_file', "ملف جديد");
--- lang/Portuguese/modules/editconfigfiles.php
+++ lang/Portuguese/modules/editconfigfiles.php
@@ -2,7 +2,7 @@
 
 define('configuration_files', "Configuração de Ficheiros");
 define('editing_file', "Editar ficheiro");
-define('no_server_specfied', "Não foi identificada nenhum ID de servidor especificado - não pode listar nenhum arquivo de configuração.");
+define('no_server_specfied', "Não foi possível identificada o ID do servidor especificado - assim sendo, não será possível mostrar algum tipo de arquivo de configuração.");
 define('no_home', "Você não tem acesso ao ID do servidor fornecido.");
 define('no_configs_for_game', "Não tem arquivos de configuração definidos.");
 define('name', "Nome");
@@ -10,8 +10,8 @@ define('no_description', "Sem Descrição");
 define('description', "Descrição");
 define('invalid_file', "O arquivo especificado pode não ser editado.");
 define('wrote_changes', "Arquivo guardado com sucesso.");
-define('failed_write', "Falha ao salvar o arquivo.");
+define('failed_write', "Falha ao guardar o arquivo.");
 define('failed_read', "Falha ao ler o arquivo.");
 define('save', "Guardar");
 define('go_back', "Voltar atrás ");
-define('new_file', "Novo arquivo");
+define('new_file', "Novo ficheiro");
