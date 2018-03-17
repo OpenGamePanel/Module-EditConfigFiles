00017f2f543177b8160496204cdbf45e4742982b
own3mall <own3mall@gmail.com>
2018-03-17 15:28:55 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Czech(CzechRepublic)/modules/editconfigfiles.php
@@ -0,0 +1,17 @@
+<?php
+
+define('OGP_LANG_configuration_files', "Konfigurační soubory");
+define('OGP_LANG_editing_file', "Úpravy souboru");
+define('OGP_LANG_no_server_specfied', "Nebyl zadán žádný ID serveru - nelze uvést žádné konfigurační soubory.");
+define('OGP_LANG_no_home', "Nemáte přístup k danému ID serveru.");
+define('OGP_LANG_no_configs_for_game', "To nemá definované konfigurační soubory.");
+define('OGP_LANG_name', "Název");
+define('OGP_LANG_no_description', "Bez popisu");
+define('OGP_LANG_description', "Popis");
+define('OGP_LANG_invalid_file', "Zadaný soubor nemusí být upravován.");
+define('OGP_LANG_wrote_changes', "Soubor byl úspěšně uložen.");
+define('OGP_LANG_failed_write', "Soubor se nezdařil.");
+define('OGP_LANG_failed_read', "Soubor se nepodařilo přečíst.");
+define('OGP_LANG_save', "Uložit");
+define('OGP_LANG_go_back', "Vraťte se zpět");
+define('OGP_LANG_new_file', "Nový soubor");
--- lang/Russian/modules/editconfigfiles.php
+++ lang/Russian/modules/editconfigfiles.php
@@ -1,17 +1,17 @@
 <?php
 
-define('OGP_LANG_configuration_files', "Configuration Files");
-define('OGP_LANG_editing_file', "Editing File");
-define('OGP_LANG_no_server_specfied', "No Server ID specified - can't list any configuration files.");
-define('OGP_LANG_no_home', "You don't have access to the given Server ID.");
-define('OGP_LANG_no_configs_for_game', "This has no configuration files defined.");
-define('OGP_LANG_name', "Name");
-define('OGP_LANG_no_description', "No Description");
-define('OGP_LANG_description', "Description");
-define('OGP_LANG_invalid_file', "The specified file may not be edited.");
-define('OGP_LANG_wrote_changes', "Successfully saved the file.");
-define('OGP_LANG_failed_write', "Failed to save the file.");
-define('OGP_LANG_failed_read', "Failed to read the file.");
-define('OGP_LANG_save', "Save");
-define('OGP_LANG_go_back', "Go Back");
-define('OGP_LANG_new_file', "New File");
+define('OGP_LANG_configuration_files', "Конфигурация файлов");
+define('OGP_LANG_editing_file', "Редактирование Файла");
+define('OGP_LANG_no_server_specfied', "Идентификатор сервера не указан. Невозможно обнаружить  файлы конфигурации.");
+define('OGP_LANG_no_home', "У вас нет доступа к данному серверу.");
+define('OGP_LANG_no_configs_for_game', "Отсутствуют конфигурационные файлы");
+define('OGP_LANG_name', "Имя");
+define('OGP_LANG_no_description', "Отсутствует описание");
+define('OGP_LANG_description', "Описание");
+define('OGP_LANG_invalid_file', "Указанный файл не может быть отредактирован.");
+define('OGP_LANG_wrote_changes', "Успешно сохранен файл.");
+define('OGP_LANG_failed_write', "Не удалось сохранить файл");
+define('OGP_LANG_failed_read', "Не удалось прочитать файл");
+define('OGP_LANG_save', "Сохранить");
+define('OGP_LANG_go_back', "Назад");
+define('OGP_LANG_new_file', "Новый файл");
