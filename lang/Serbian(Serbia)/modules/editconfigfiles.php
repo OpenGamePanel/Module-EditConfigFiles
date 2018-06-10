108703d03fade2dd22977ecd0d3afc230c61f811
own3mall <own3mall@gmail.com>
2018-06-10 09:00:26 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Serbian(Serbia)/modules/editconfigfiles.php
@@ -0,0 +1,17 @@
+<?php
+
+define('OGP_LANG_configuration_files', "Конфигурациони фајлови");
+define('OGP_LANG_editing_file', "Уређивање фајлова");
+define('OGP_LANG_no_server_specfied', "Није наведен ID сервера - није могуће приказати конфигурационе фајлове.");
+define('OGP_LANG_no_home', "Немате приступ датом ID-у сервера.");
+define('OGP_LANG_no_configs_for_game', "Нема дефинисаних конфигурационих фајлова.");
+define('OGP_LANG_name', "Име");
+define('OGP_LANG_no_description', "Без описа");
+define('OGP_LANG_description', "Опис");
+define('OGP_LANG_invalid_file', "Наведени фајл можда неће бити уређиван.");
+define('OGP_LANG_wrote_changes', "Фајл је успешно сачуван.");
+define('OGP_LANG_failed_write', "Учитавање фајла није успело.");
+define('OGP_LANG_failed_read', "Читање фајла није успело.");
+define('OGP_LANG_save', "Сачувај");
+define('OGP_LANG_go_back', "Иди назад");
+define('OGP_LANG_new_file', "Нови фајл");
