fe09a2bacf62dbfa4963f3e728cd60489b1d649d
own3mall <own3mall@gmail.com>
2018-06-18 13:28:45 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Turkish(Turkey)/modules/editconfigfiles.php
+++ lang/Turkish(Turkey)/modules/editconfigfiles.php
@@ -1,17 +1,17 @@
 <?php
 
-define('OGP_LANG_configuration_files', "Configuration Files");
-define('OGP_LANG_editing_file', "Editing File");
+define('OGP_LANG_configuration_files', "Konfigürsayon Dosyaları");
+define('OGP_LANG_editing_file', "Dosya Düzenleniyor");
 define('OGP_LANG_no_server_specfied', "No Server ID specified - can't list any configuration files.");
 define('OGP_LANG_no_home', "You don't have access to the given Server ID.");
-define('OGP_LANG_no_configs_for_game', "This has no configuration files defined.");
-define('OGP_LANG_name', "Name");
-define('OGP_LANG_no_description', "No Description");
-define('OGP_LANG_description', "Description");
-define('OGP_LANG_invalid_file', "The specified file may not be edited.");
-define('OGP_LANG_wrote_changes', "Successfully saved the file.");
-define('OGP_LANG_failed_write', "Failed to save the file.");
-define('OGP_LANG_failed_read', "Failed to read the file.");
-define('OGP_LANG_save', "Save");
-define('OGP_LANG_go_back', "Go Back");
-define('OGP_LANG_new_file', "New File");
+define('OGP_LANG_no_configs_for_game', "Bunun tanınlanmış konfigürasyon dosyası yok.");
+define('OGP_LANG_name', "Ad");
+define('OGP_LANG_no_description', "Açıklama Yok");
+define('OGP_LANG_description', "Açıklama");
+define('OGP_LANG_invalid_file', "Belirlenmiş dosyalar düzenlenemez.");
+define('OGP_LANG_wrote_changes', "Dosya başarıyla kaydedildi.");
+define('OGP_LANG_failed_write', "Dosya kaydı başarısız.");
+define('OGP_LANG_failed_read', "Dosya okunması başarısız.");
+define('OGP_LANG_save', "Kaydet");
+define('OGP_LANG_go_back', "Geri Dön");
+define('OGP_LANG_new_file', "Yeni Dosya");
