fa9fb00c89024bb75e5e22f86ce65e08dc0287bb
own3mall <own3mall@gmail.com>
2018-12-13 06:57:40 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Danish/modules/editconfigfiles.php
+++ lang/Danish/modules/editconfigfiles.php
@@ -1,17 +1,17 @@
 <?php
 
-define('OGP_LANG_configuration_files', "Configuration Files");
-define('OGP_LANG_editing_file', "Editing File");
-define('OGP_LANG_no_server_specfied', "No Server ID specified - can't list any configuration files.");
-define('OGP_LANG_no_home', "You don't have access to the given Server ID.");
-define('OGP_LANG_no_configs_for_game', "This has no configuration files defined.");
-define('OGP_LANG_name', "Name");
-define('OGP_LANG_no_description', "No Description");
-define('OGP_LANG_description', "Description");
-define('OGP_LANG_invalid_file', "The specified file may not be edited.");
-define('OGP_LANG_wrote_changes', "Successfully saved the file.");
-define('OGP_LANG_failed_write', "Failed to save the file.");
-define('OGP_LANG_failed_read', "Failed to read the file.");
-define('OGP_LANG_save', "Save");
-define('OGP_LANG_go_back', "Go Back");
-define('OGP_LANG_new_file', "New File");
+define('OGP_LANG_configuration_files', "Konfigurations filer");
+define('OGP_LANG_editing_file', "Redigere fil");
+define('OGP_LANG_no_server_specfied', "Ingen Server ID specificeret - Kan ikke visse nogen konfigurations filer.");
+define('OGP_LANG_no_home', "Du har ikke adgang til det givne Server ID.");
+define('OGP_LANG_no_configs_for_game', "Dette har ingen konfigurations filer defineret.");
+define('OGP_LANG_name', "Navn");
+define('OGP_LANG_no_description', "In Beskrivelse");
+define('OGP_LANG_description', "Beskrivelse");
+define('OGP_LANG_invalid_file', "Den angivne fil må ikke redigeres");
+define('OGP_LANG_wrote_changes', "Gemt filen succesfuldt.");
+define('OGP_LANG_failed_write', "Fejlede i at gemme filen.");
+define('OGP_LANG_failed_read', "Fejlede i at læse filen.");
+define('OGP_LANG_save', "Gem");
+define('OGP_LANG_go_back', "Gå tilbage");
+define('OGP_LANG_new_file', "Ny fil");
