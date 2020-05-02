fd786248fb002358937cb29f605937e8b4ac40a0
own3mall <own3mall@gmail.com>
2020-05-02 03:58:43 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Hebrew(Israel)/modules/editconfigfiles.php
+++ lang/Hebrew(Israel)/modules/editconfigfiles.php
@@ -1,17 +1,17 @@
 <?php
 
-define('OGP_LANG_configuration_files', "Configuration Files");
-define('OGP_LANG_editing_file', "Editing File");
-define('OGP_LANG_no_server_specfied', "No Server ID specified - can't list any configuration files.");
-define('OGP_LANG_no_home', "You don't have access to the given Server ID.");
-define('OGP_LANG_no_configs_for_game', "This has no configuration files defined.");
-define('OGP_LANG_name', "Name");
-define('OGP_LANG_no_description', "No Description");
-define('OGP_LANG_description', "Description");
-define('OGP_LANG_invalid_file', "The specified file may not be edited.");
-define('OGP_LANG_wrote_changes', "Successfully saved the file.");
-define('OGP_LANG_failed_write', "Failed to save the file.");
-define('OGP_LANG_failed_read', "Failed to read the file.");
-define('OGP_LANG_save', "Save");
-define('OGP_LANG_go_back', "Go Back");
-define('OGP_LANG_new_file', "New File");
+define('OGP_LANG_configuration_files', "קבצי תצורה");
+define('OGP_LANG_editing_file', "עריכת קבצים");
+define('OGP_LANG_no_server_specfied', "לא צוין מזהה שרת - לא ניתן לרשום קבצי תצורה.");
+define('OGP_LANG_no_home', "אין לך גישה למזהה השרת הנתון.");
+define('OGP_LANG_no_configs_for_game', "אין להגדרת קבצי תצורה זו.");
+define('OGP_LANG_name', "שם");
+define('OGP_LANG_no_description', "אין תיאור");
+define('OGP_LANG_description', "תיאור");
+define('OGP_LANG_invalid_file', "יתכן שלא ניתן לערוך את הקובץ שצוין");
+define('OGP_LANG_wrote_changes', "שמירת הקובץ הושלמה");
+define('OGP_LANG_failed_write', "שמירת הקובץ נכשלה");
+define('OGP_LANG_failed_read', "קריאת הקובץ נכשלה");
+define('OGP_LANG_save', "שמירה");
+define('OGP_LANG_go_back', "חזור אחורה");
+define('OGP_LANG_new_file', "קובץ חדש");
