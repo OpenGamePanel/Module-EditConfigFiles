90b6e175f766215b551570ad683b46313d74df21
own3mall <own3mall@gmail.com>
2021-11-23 02:52:42 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Italian/modules/editconfigfiles.php
+++ lang/Italian/modules/editconfigfiles.php
@@ -1,13 +1,13 @@
 <?php
 
-define('OGP_LANG_configuration_files', "Configuration Files");
+define('OGP_LANG_configuration_files', "File di configurazione");
 define('OGP_LANG_editing_file', "Editing File");
 define('OGP_LANG_no_server_specfied', "No Server ID specified - can't list any configuration files.");
 define('OGP_LANG_no_home', "You don't have access to the given Server ID.");
 define('OGP_LANG_no_configs_for_game', "This has no configuration files defined.");
-define('OGP_LANG_name', "Name");
-define('OGP_LANG_no_description', "No Description");
-define('OGP_LANG_description', "Description");
+define('OGP_LANG_name', "Nome");
+define('OGP_LANG_no_description', "Nessuna descrizione");
+define('OGP_LANG_description', "Descrizione");
 define('OGP_LANG_invalid_file', "The specified file may not be edited.");
 define('OGP_LANG_wrote_changes', "Successfully saved the file.");
 define('OGP_LANG_failed_write', "Failed to save the file.");
