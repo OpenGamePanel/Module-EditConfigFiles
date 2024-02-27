bf05ab957f48fdb93dbac391f37d87cdeed695cc
own3mall <own3mall@gmail.com>
2024-02-26 21:22:36 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Chinese(China)/modules/editconfigfiles.php
@@ -0,0 +1,17 @@
+<?php
+
+define('OGP_LANG_configuration_files', "配置文件");
+define('OGP_LANG_editing_file', "编辑文件");
+define('OGP_LANG_no_server_specfied', "未指定服务器");
+define('OGP_LANG_no_home', "没有主目录");
+define('OGP_LANG_no_configs_for_game', "此游戏无配置文件");
+define('OGP_LANG_name', "名称");
+define('OGP_LANG_no_description', "无描述");
+define('OGP_LANG_description', "描述");
+define('OGP_LANG_invalid_file', "无效文件");
+define('OGP_LANG_wrote_changes', "写入更改成功");
+define('OGP_LANG_failed_write', "写入失败");
+define('OGP_LANG_failed_read', "读取失败");
+define('OGP_LANG_save', "保存");
+define('OGP_LANG_go_back', "返回");
+define('OGP_LANG_new_file', "新文件");
